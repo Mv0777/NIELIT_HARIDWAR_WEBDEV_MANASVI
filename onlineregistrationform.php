c94f5b227e58ec6c0f37cba7ba548bd709e5bd3b
Mv0777 <89659002+Mv0777@users.noreply.github.com>
2021-09-01 20:36:38 +0530

    Add files via upload

--- onlineregistrationform.php
+++ onlineregistrationform.php
@@ -8,9 +8,28 @@
          background-image: url(haridwar-to-badrinath-4348954.jpg);
          background-size: initial;
      }	
-     /*#table{
-     	background-color: lemonchiffon;
-     }*/
+     table{
+     	padding: 0PX;
+     	margin: 0PX;
+     	font-size: xx-large;
+     	font-weight: 900;
+     	width: 700px;
+     	font-family: xx-large;
+     }
+     FIELDSET{
+		height: pX;
+		width: PX;
+			background-color: ;		
+		color:BLACK ;
+		font-size: xx-large;}
+
+		#a,#b,#c,#d,#e{text-field: 50px ;
+			height: 50px;
+			font-size: xx-large;}
+			#f,#g{
+				font-size: xx-large;
+			}
+
 	</style>
 	<script type="text/javascript">
 		function display()
@@ -25,17 +44,21 @@
 		}
 	</script>
 </head>
-<body><center style="background-color: lemonchiffon;"><form method="post" action="process.php" ><fieldset>
-	<table  border="2" cellpadding="0" cellspacing="0" > 
-							<div><h4><u><I>BUS RESERVATION SYSTEM</I></u></h4></div>	
+<body><center style="; background-color: lemonchiffon;"><form method="post" action="process.php" >
+	<fieldset>
+	<table  border="2" cellpadding="0" cellspacing="0" height="200px" width="800px" > 
+							<div><h1><u><I>BUS RESERVATION SYSTEM</I></u></h1></div>	
 
 								
-<tr><td>source</td><td><input type="text" name="source" placeholder="source" required></td></tr>
-<tr><td>destination</td><td><input type="text" name="destination" placeholder="destination" required>
-<tr><td>departure</td><td><input type="text" name="departure" placeholder="DDMMYYYY"  required></td></tr>
-<tr><td>coming</td><td><input type="text" name="coming" placeholder="DDMMYYYY"  required></td></tr>
+<tr><td>source</td><td><input type="text" name="source"  
+	size="32px" placeholder="source" id="d" required></td></tr>
+<tr><td>destination</td><td width="200PX"><input type="text" size="32px" name="destination" placeholder="destination" width="600px" height="600px" id="c" required>
+<tr><td>departure</td><td><input type="text" size="32px" name="departure" placeholder="DDMMYYYY" id="b" required></td></tr>
+<tr><td>coming</td><td ><input type="text" size="32px"  name="coming" placeholder="DDMMYYYY" id="a" required></td></tr>
 <tr><td>numberofseats</td><TD>
-<select name="numberofseats" required>
+<select name="numberofseats" id="e" required>
+	<option value=""></option>
+
 <option value="1">1</option>
 <option value="2">2</option>  
 <option value="3">3</option>
@@ -43,9 +66,9 @@
 <option value="5">5</option>
 <option value="6">6</option> 
 </select></td></tr>
-<tr><td><input type="submit" name="submit"  value="submit" onclick="displayone()"></td></tr>
+<tr><td><input type="submit" name="submit"  value="submit" onclick="displayone()" id="f"></td></tr>
 
-<input type="button" onclick="display()" value="click me"></script>
+<input type="button" onclick="display()" value="click me" id="g">
 </div>
 </table></fieldset></form></center>
 </body>
