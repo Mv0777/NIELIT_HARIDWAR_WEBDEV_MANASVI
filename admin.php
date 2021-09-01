7326be2713343e4e39dceb0f2d45dda2dd5a4be6
Mv0777 <89659002+Mv0777@users.noreply.github.com>
2021-09-01 20:17:42 +0530

    Add files via upload
    This is the edited version of login page.Thanks

--- admin.php
+++ admin.php
@@ -1,22 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>ADMINISTRATOR</title>
-<style>
+	<title>login page</title>
+	<style>
 		body{
 		
 			background-image: radial-gradient(circle,red,green,blue,yellow);		
 		}
 	</style>
-
 </head>
-<body >			
-
-<form method="POST" action="database.php"><fieldset> 
-username<input type="text" name="username">
-Password<input type="password" name="password">
-<input type="submit" name="submit" value="submit">
-</fieldset></form>
+<body>
+<center>
+	<form method="post" action=""><table>
+		<tr><td>username:</td><td><input type="text" name="username" placeholder="enter username"></td></tr>
+		<tr><td>password:</td><td><input type="password" name="password" placeholder="enter password"></td></tr>
+		<tr><td></td><td><input type="submit" name="submit" value="login"></td></tr>
+	</table>
+</form>
 </body>
 </html>
+<?php
+if(isset($_POST['submit']))
+{
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	if($username=="admin" && $password=="12345")
+	{
+		echo "<script>alert('login ok')</script>";
+		echo "<script> window.open('conditional.php')</script>";
+	}
+	else
+	{
+		echo "<script>alert('wrong username and password')</script>";
+	}
+}
+?>
