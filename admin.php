0e744e96607098bc7edc4ca70a72f6e56d5036ac
Mv0777 <89659002+Mv0777@users.noreply.github.com>
2021-09-02 15:45:58 +0530

    Add files via upload
    Final edition of login page.

--- admin.php
+++ admin.php
@@ -9,29 +9,37 @@
 		}
 	</style>
 </head>
+
 <body>
-<center>
-	<form method="post" action=""><table>
-		<tr><td>username:</td><td><input type="text" name="username" placeholder="enter username"></td></tr>
-		<tr><td>password:</td><td><input type="password" name="password" placeholder="enter password"></td></tr>
-		<tr><td></td><td><input type="submit" name="submit" value="login"></td></tr>
-	</table>
+<center><form method="post" action="search1.php"><table>
+	<tr><td>username:</td><td><input type="text" name="username" placeholder="enter username" required></td><td><input type="submit" name="submit" value="Search"></td></tr>
+</table>
 </form>
+</center>
 </body>
 </html>
 <?php
+include 'database.php';
 if(isset($_POST['submit']))
-{
+{ 
 	$username=$_POST['username'];
-	$password=$_POST['password'];
-	if($username=="admin" && $password=="12345")
+	$sql="select * from admin where username='$username'";
+	$que=mysqli_query($con,$sql);
+	if(mysqli_num_rows($que)>0)
+{
+	echo "<table border='2' style='background-color:red;'><tr><th>username</th><th>password</th></tr>"; /*<th>dob</th><th>Gender</th><th>education</th></tr>";*/
+
+	while($row=mysqli_fetch_assoc($que))
 	{
-		echo "<script>alert('login ok')</script>";
-		echo "<script> window.open('conditional.php')</script>";
+				echo "<tr><td>".$row["username"]."</td><td>".$row["password"]."</td></tr>"; 
+				/*.$row["dob"]."</td><td>".$row["gender"]."</td><td>".$row["education"]."</td></tr>";*/
 	}
-	else
-	{
-		echo "<script>alert('wrong username and password')</script>";
-	}
+	echo "</table>";
+}
+else
+{
+	echo "data is not available in table";
+}
+mysqli_close($con);
 }
 ?>
